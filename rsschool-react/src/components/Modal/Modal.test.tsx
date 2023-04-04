76614470186b669a574883e5ee208d5316b3b0b6
Alick Shabanovich <alikshabanovich@gmail.com>
2023-04-05 01:33:00 +0300

    feat: add Modal component test

---
+++ rsschool-react/src/components/Modal/Modal.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Modal from './Modal'
+import { vi } from 'vitest'
+const func = vi.fn()
+describe('Modal component', () => {
+    it('Modal render', () => {
+        render(<Modal onClose={func} children={undefined} />)
+        expect(screen.getByRole('dialog')).toBeInTheDocument()
+    })
+})
