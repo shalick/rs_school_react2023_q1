8135bb22fc6a2f18759dfff63f589260e6234fa3
Alick Shabanovich <alikshabanovich@gmail.com>
2023-04-19 21:29:47 +0300

    fix: lint warnings

--- rsschool-react/src/App.test.tsx
+++ rsschool-react/src/App.test.tsx
@@ -1,11 +1,5 @@
 import { describe, it } from 'vitest'
-import {
-    render,
-    screen,
-    fireEvent,
-    waitForElementToBeRemoved,
-} from '@testing-library/react'
-import React from 'react'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 import { Provider } from 'react-redux'
 import { setupStore } from './store/store'
